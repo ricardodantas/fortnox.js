942c9e80d52e67a6ff12bf7ea991ae40be744586
Ricardo Dantas <ricardodantas@gmail.com>
2019-01-04 14:53:35 +0100

    fix webpack and babel ingegration

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,21 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'production',
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'fortnoxab-api-js-client.js',
     path: path.resolve(__dirname, 'lib')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
   }
 };
